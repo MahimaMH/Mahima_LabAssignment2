e2c100f4309cfdf2fb17afeee199c32779eb29d8
MahimaMH <80575598+MahimaMH@users.noreply.github.com>
2021-03-19 12:21:20 +0530

    Update ArrayAssignment8.java

--- ArrayAssignment8.java
+++ ArrayAssignment8.java
@@ -2,14 +2,14 @@
 /**
  * Nmae:Mahima M Hampali
  * Description: program to convert array elements to uppercase
- * Date :170/21
+ * Date :17/03/21
   */
 
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class ArrayAssignment8 {
-	//private static String[] arr= {"Soap","Sugar","Teapowder","Oil","Soap","Teapowder"};
+	
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
 		System.out.println("Enter number of elemenets in an array: ");
@@ -18,32 +18,45 @@ public class ArrayAssignment8 {
 		System.out.println("Enter elemenets of an array: ");
 		for(int i=0;i<n;i++) {
 			
-			arr[i]=sc.toString();
+			arr[i]=sc.next();
 			
 		}
 		for(int i=0;i<n;i++) {
 		System.out.println(arr[i]);
 		}
-		sort();
-		upperCase();
+		sort(arr,n);
+		upperCase(arr,n);
 
 	}
-	public static void sort()
+	public static void sort(String[] arr, int n)
 	{
-		char[][] arr = null;
-		Arrays.sort(arr,0,6);
+		Arrays.sort(arr,0,n);
 	
-	for(int i=0;i<arr.length;i++) {
-		System.out.println(arr[i]);
-		
-	}
-}
-	public static void upperCase() {
-		
-		String[][] arr;
 		for(int i=0;i<arr.length;i++) {
+		System.out.println(arr[i]);
+		}
+	}
+	public static void upperCase(String[] arr, int n) {
+		if(n%2==0) {
+			for(int i=0;i<((arr.length)/2);i++) {
 			arr[i] = arr[i].toUpperCase();
 			System.out.println(arr[i]);
+			}
+			for(int i=((arr.length)/2);i<=arr.length;i++) {
+			arr[i] = arr[i].toLowerCase();
+			System.out.println(arr[i]);
+			}
+			
 		}
+		else{
+			for(int i=0;i<(((arr.length)+1)/2);i++) {
+				arr[i] = arr[i].toUpperCase();
+				System.out.println(arr[i]);
+				}
+			for(int i=(((arr.length)+1)/2);i<=arr.length;i++) {
+				arr[i] = arr[i].toLowerCase();
+				System.out.println(arr[i]);
+				}
+		}				
 	}
 }
